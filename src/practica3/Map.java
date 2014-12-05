657cb044d9614fd474f1a52226e8a25f5d7edf41
antonio <antonio@mosquito>
2014-12-05 16:52:33 +0100

    Esqueleto Map Javi Ortega

---
+++ src/practica3/Map.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practica3;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author 
+ */
+public class Map {
+    private HashMap map;
+    
+    public Map(){
+        this.map = new HashMap<Coord, Integer>();
+    }
+}
