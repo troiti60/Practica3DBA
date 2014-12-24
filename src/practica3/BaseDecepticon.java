f8ed9c4fb60c4ee0e8dde84d171a75ec69d3b846
rod <rod@rod-PC>
2014-12-24 11:05:18 +0100

    BaseDecepticon - Añadida conexion servidor

--- src/practica3/BaseDecepticon.java
+++ src/practica3/BaseDecepticon.java
@@ -7,6 +7,7 @@
 package practica3;
 
 import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.AgentsConnection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,18 +22,32 @@ public class BaseDecepticon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("BaseDecepticon: Iniciando...");
+        
         DataAccess data = DataAccess.crearInstancia();
+        
+        // Conection with server
+        AgentsConnection.connect(data.getHost(), data.getPort(),
+                data.getVirtualHost(), data.getUsername(),
+                data.getPassword(), data.getSSL());
+        
+        System.out.println("BaseDecepticon: Creada conexión con el servidor");
+        
         AgentID idMega = new AgentID( DataAccess.getNameMegatron() );
         Megatron mega = null;
         
         try {
+            System.out.println("BaseDecepticon: Instanciando Megatron");
             mega = new Megatron(idMega);
         } catch (Exception ex) {
             Logger.getLogger(BaseDecepticon.class.getName()).log(Level.SEVERE, null, ex);
-            System.err.println("Error al instanciar Megatron");
+            System.err.println("\n####################\nError al instanciar Megatron\n####################\n");
         }
         
+        System.out.println("BaseDecepticon: Lanzando Megatron");
         mega.start();
+        System.out.println("BaseDecepticon: Fin");
     }
     
 }
