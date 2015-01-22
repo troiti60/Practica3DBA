da61fda7b050a1593a8298d2f5f68b9740510617
rod <rod@rod-PC>
2015-01-22 13:13:09 +0100

    DataAcces - modificaciones

--- src/practica3/DataAccess.java
+++ src/practica3/DataAccess.java
@@ -61,14 +61,14 @@ public class DataAccess {
         this.username = "Capricornio";
         this.passwd = "Gaugin";
         this.ssl = false;
-        //this.world  = "beach";
-        //this.world  = "pyrenees";
-        //this.world  = "alps";
+        //this.world  = "beach"; // OK
+        //this.world  = "pyrenees"; //ok
+        this.world  = "alps";
         //this.world  = "andes";
         //this.world  = "valleys";
         //this.world  = "faun";
         //this.world  = "everest";
-        this.world  = "newyork";
+        //this.world  = "newyork";
         this.key = null;
     }
 
